75f597e88887fee4eae7a6d6d55eb09c35c2092f
Riku458 <jedrickpogi69@gmail.com>
2025-04-10 19:43:40 +0800

    Pseudocode pt. 2 (Since I realized that I need to use while loop in the whole code)

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,8 @@
 # Assignment No.09: Quiz Creator
 
-#1. Get the questions that the user wants
-#2. Get the options that the user will put in the question and the correct answer
+#1. Get the questions that the user wants (I will use while loop here also)
+
+
+
+#2. Get the options that the user will put in the question and the correct answer (I will use while loop here also)
 #3. Get the file of the questions using while loop
