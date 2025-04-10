df71de7be6603b60d05932f74480619ea797d0ff
Riku458 <jedrickpogi69@gmail.com>
2025-04-11 01:05:08 +0800

    changing the name of the variable + making a third test

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,17 +1,17 @@
 # Assignment No.09: Quiz Creator
 
-file = input("Enter a file name for your quiz file (without its extenstion): ") + ".txt" 
+file_name = input("Enter a file name for your quiz file (without its extenstion): ") + ".txt" 
 
 print("\n--------QUIZ MAKER--------")
 #1. Get the questions that the user wants (I will use while loop here also)
 
-with open(file, "a") as f:  #3. Get the file of the questions using while loop
+with open(file_name, "a") as file:      #3. Get the file of the questions using while loop
     while True:
         question = input("Enter question (or enter 'quit' to exit): ")
         if question.lower() == "quit":
             break
     
-        options = []             #2. Get the options that the user will put in the question and the correct answer (I will use while loop here also)
+        options = []            #2. Get the options that the user will put in the question and the correct answer (I will use while loop here also)
         for letter in ["a", "b", "c", "d"]:
             options.append(input(f"Enter option {letter}: "))
 
@@ -20,10 +20,10 @@ with open(file, "a") as f:  #3. Get the file of the questions using while loop
             print("Please enter a, b, c, or d")
             correct = input("Correct answer (a-d): ").lower()
 
-        f.write(f"Q: {question}\n")
+        file.write(f"Q: {question}\n")
         for i, option in enumerate(options):
-            f.write(f"{chr(97+i)}) {option}\n")
-        f.write(f"Correct: {correct}\n\n")
+            file.write(f"{chr(97+i)}) {option}\n")
+        file.write(f"Correct: {correct}\n\n")
 
         while True:
             choice = input("Add another question? (y/n): ").lower()
@@ -34,5 +34,5 @@ with open(file, "a") as f:  #3. Get the file of the questions using while loop
         if choice == "n":
             break
 
-print(f"\nAll questions saved to {file}")
+print(f"\nAll questions saved to {file_name}")
 
