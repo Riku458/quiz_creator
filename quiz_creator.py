34db5fb9d4f87108dea73a1f7cc1eef2f687879b
Riku458 <jedrickpogi69@gmail.com>
2025-04-10 19:41:33 +0800

    Pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,5 @@
+# Assignment No.09: Quiz Creator
+
+#1. Get the questions that the user wants
+#2. Get the options that the user will put in the question and the correct answer
+#3. Get the file of the questions using while loop
