30915609005a4b08d796374adf6ccb64c28a4e07
Riku458 <jedrickpogi69@gmail.com>
2025-04-11 00:03:14 +0800

    making + testing if the quiz file is being save or working as intended

--- quiz_creator.py
+++ quiz_creator.py
@@ -4,17 +4,27 @@
 print("--------QUIZ MAKER--------")
 #1. Get the questions that the user wants (I will use while loop here also)
 
-while True:
-    question = input("Enter question (or enter 'quit' to exit): ")
-    if question.lower() == "quit":
-        break
+with open("quiz.txt", "a") as f:  #3. Get the file of the questions using while loop
+    while True:
+        question = input("Enter question (or enter 'quit' to exit): ")
+        if question.lower() == "quit":
+            break
     
-    option = []             #2. Get the options that the user will put in the question and the correct answer (I will use while loop here also)
-    for letter in ["a", "b", "c", "d"]:
-        option.append(input(f"Enter option {letter}: "))
+        options = []             #2. Get the options that the user will put in the question and the correct answer (I will use while loop here also)
+        for letter in ["a", "b", "c", "d"]:
+            options.append(input(f"Enter option {letter}: "))
 
-    correct = input("Correct answer (a-d): ").lower()
-    while correct not in ["a", "b", "c", "d"]:
-        print("Please enter a, b, c, or d")
         correct = input("Correct answer (a-d): ").lower()
-#3. Get the file of the questions using while loop
+        while correct not in ["a", "b", "c", "d"]:
+            print("Please enter a, b, c, or d")
+            correct = input("Correct answer (a-d): ").lower()
+
+        f.write(f"Q: {question}\n")
+        for i, option in enumerate(options):
+            f.write(f"{chr(97+i)}) {option}\n")
+        f.write(f"Correct: {correct}\n\n")
+
+        print("Question Saved!\n")
+
+print("\nAll questions saved to quiz.txt")
+
