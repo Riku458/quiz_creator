cdcc9b65f2fb03f497ed13d22d9a87f49f252058
Riku458 <jedrickpogi69@gmail.com>
2025-04-10 19:51:20 +0800

    making a header + testing if the loop for question is working as intended

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,13 @@
 # Assignment No.09: Quiz Creator
 
+
+print("--------QUIZ MAKER--------")
 #1. Get the questions that the user wants (I will use while loop here also)
 
-
+while True:
+    question = input("Enter question (or enter 'quit' to exit): ")
+    if question.lower() == "quit":
+        break
 
 #2. Get the options that the user will put in the question and the correct answer (I will use while loop here also)
 #3. Get the file of the questions using while loop
