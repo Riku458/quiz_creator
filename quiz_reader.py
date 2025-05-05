6eda9e7788c1e43fc05587c26144cecaa7a77988
Riku458 <jedrickpogi69@gmail.com>
2025-05-05 17:41:16 +0800

    making a loop for the menu section

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,4 +1,18 @@
+import random
+
 # ASSIGNMENT NO.10: QUIZ READER FROM QUIZ CREATOR
 
+
 #1. Make a menu for the user to choose either making a quiz or taking a quiz
+
+print("\n-------- QUIZ PROGRAM --------")
+
+while True:
+    print("\n-------- MAIN MENU --------")
+    print("1. Create a new quiz")
+    print("2. Take a quiz")
+    print("3. Exit")
+
+    choice = input("Enter your choice (1-3): ")
+
 #2. Make a function that will align in the menu
